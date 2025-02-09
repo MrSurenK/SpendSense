87f93dd38052d29fbee49daa75b9910b744f37cb
MrSurenK <surenkworkz@gmail.com>
2025-02-09 17:57:38 +0800

    Project files

---
+++ SpendSense-BackEnd/src/main/java/com/MrSurenK/SpendSense_BackEnd/SpendSenseBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.MrSurenK.SpendSense_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendSenseBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendSenseBackEndApplication.class, args);
+	}
+}
---
+++ SpendSense-BackEnd/src/test/java/com/MrSurenK/SpendSense_BackEnd/SpendSenseBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MrSurenK.SpendSense_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendSenseBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
