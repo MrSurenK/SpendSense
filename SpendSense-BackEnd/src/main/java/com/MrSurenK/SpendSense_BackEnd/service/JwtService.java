486f3791c9366fd2d473d30a42a274c7e0ad58f8
MrSurenK <surenkworkz@gmail.com>
2025-06-17 23:47:44 +0800

    Writing JWT service

--- SpendSense-BackEnd/src/main/java/com/MrSurenK/SpendSense_BackEnd/service/JwtService.java
+++ SpendSense-BackEnd/src/main/java/com/MrSurenK/SpendSense_BackEnd/service/JwtService.java
@@ -1,6 +1,52 @@
 package com.MrSurenK.SpendSense_BackEnd.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parser()                       //Step 1: Get a JwtParserBuilder
+                .verifyWith(getSignInKey())     //Step 2: Set the signing Key
+                .build()                        //Step 3: Build the JWtParser
+                .parseSignedClaims(token)       //Step 4: Parse the signed JWT (String implements CharSequence)
+                .getPayload();                  //Step 5: Extract the claims
+    }
+
+    private SecretKey getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+
+
+
 
 
 }
