206fd1bf7c3562d68d7c26e81ac7673691f0af50
suren <surenkworkz@gmail.com>
2025-11-06 16:08:14 +0800

    implemented redux-persist to persist auth state for cleaning UX experience

--- SpendSense-FrontEnd/SpendSense/src/main.tsx
+++ SpendSense-FrontEnd/SpendSense/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { store } from "./redux/store.ts";
+import { store, persistor } from "./redux/store.ts";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 import App from "./App.tsx";
@@ -9,7 +10,9 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- SpendSense-FrontEnd/SpendSense/src/redux/store.ts
+++ SpendSense-FrontEnd/SpendSense/src/redux/store.ts
@@ -1,18 +1,48 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { authApi } from "./rtk-queries/authService";
 import authStatusReducer from "../redux/slices/authSlice";
 import { dashboardApi } from "./rtk-queries/dashboardService";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/es/storage";
+
+// ToDo: Redux-persisit configuration so that logIn persist when browser is refreshed.
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+};
+
+const rootReducer = combineReducers({
+  [authApi.reducerPath]: authApi.reducer,
+  auth: authStatusReducer,
+  [dashboardApi.reducerPath]: dashboardApi.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    [authApi.reducerPath]: authApi.reducer,
-    auth: authStatusReducer,
-    [dashboardApi.reducerPath]: dashboardApi.reducer,
-  },
-
-  middleware: (getDefaultMiddleWare) =>
-    getDefaultMiddleWare().concat(authApi.middleware, dashboardApi.middleware),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(authApi.middleware, dashboardApi.middleware),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+// middleware: (getDefaultMiddleWare) =>
+//   getDefaultMiddleWare().concat(authApi.middleware, dashboardApi.middleware),
